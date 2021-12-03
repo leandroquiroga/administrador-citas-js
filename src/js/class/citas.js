abfbe564b462d470142341d4c99b6d7a1ff37985
Leandro <leandroquiroga9514@gmail.com>
2021-12-03 12:34:42 -0300

    Add, Delete, Edit appointment

--- src/js/class/citas.js
+++ src/js/class/citas.js
@@ -2,18 +2,14 @@ export class Citas {
     constructor() {
         this._citas = []
     }
-
     // crea una nueva cita
     agragarCitas(citas) {
         this._citas = [...this._citas, citas]
     }
-
     // elimina una cita
     eliminarCita(id) {
         this._citas = this._citas.filter( cita => cita.id !== id)
     }
-
-
     // crea un nuevo arreglo en base al id actual, si es igual retorna el la cita editada sino retorna la cita actual
     reenombrarCitas(citaEditada) {
         this._citas = this._citas.map(cita => cita.id === citaEditada.id ? citaEditada : cita )
